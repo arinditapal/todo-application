26878399e004b5c4adcf47d88d78843969e54d15
Arindita Pal <arinditapal2003@gmail.com>
2023-12-19 14:26:12 +0530

    public folder, package.json  updated

--- public/scripts.js
+++ public/scripts.js
@@ -1,83 +1,122 @@
 console.log("Todo App!");
-const todoList = document.querySelector('ol');
+const todoList = document.querySelector("ol");
 const showDataButton = document.getElementById("showData");
-const form = document.querySelector('form');
-
-let todos = [
-    {
-        title: "study computer networks",
-        completed: false,
-        id: 0
-    },
-    {
-        title: "play stardew valley",
-        completed: true,
-        id: 1
-    },
-    {
-        title: "play with putui",
-        completed: false,
-        id: 2
-    },
-];
+const form = document.querySelector("form");
 
 
-showDataButton.addEventListener("click", function() {
+/** @typedef {{title: string, completed: boolean, id: number}} Todo */
+
+if (showDataButton) {
+    showDataButton.addEventListener("click", () => {
+        fetch('/api/todos/')
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err.message));
+    });
+}
+
+async function printTodos() {
+    if (!todoList) {
+        return;
+    }
+
+    todoList.innerHTML = "";
+
+    /** @type {Todo[]} */
+    const todos = await fetch("/api/todos").then((res) => res.json());
     console.log(todos);
-})
 
-function printTodos() {
-    todoList.innerHTML = '';
-    todos.forEach( todo => {
-        let listItem = document.createElement('li');
-        let title = document.createElement('span');
-        let deleteButton = document.createElement('button');
+    todos.forEach((todo) => {
+        let listItem = document.createElement("li");
+        let title = document.createElement("span");
+        let deleteButton = document.createElement("button");
 
         title.textContent = todo.title;
-        title.style = 'cursor: pointer; margin: 0 5px;';
+        // title.style = "cursor: pointer; margin: 0 5px;";
+        title.style.cursor = "pointer";
+        title.style.margin = "0 5px";
 
-        deleteButton.textContent = 'X';
+        deleteButton.textContent = "❌";
+        deleteButton.style.background = "white";
+        deleteButton.style.border = "1px solid red";
+        deleteButton.style.borderRadius = "5px";
 
-        if(todo.completed === true) {
-            title.classList.add('strike');
+        if (todo.completed === true) {
+            title.classList.add("strike");
         }
 
-        title.addEventListener('click', () => {
-            todo.completed = !todo.completed;
-            console.log("striked: ", todo); 
-            printTodos();
-        })
+        title.addEventListener("click", () => {
+            console.log(todo);
+            const url = `/api/todos/${todo.id}`;
 
-        deleteButton.addEventListener('click', () => {
-            todos.splice(todo.id, 1);
-            printTodos();
-        })
-        
+            const modifiedTodo = {
+                completed: !todo.completed
+            };
+
+            console.log(modifiedTodo);
+
+            fetch(url, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify( modifiedTodo ),
+            })
+            .then((res) => printTodos())
+            .catch((err) => console.log(err.message));
+        });
+
+        deleteButton.addEventListener("click", (e) => {
+            const url = `/api/todos/${todo.id}`;
+            fetch(url, {
+                method: "DELETE",
+            })
+                .then((res) => {
+                    console.log("deleted");
+                    printTodos();
+                })
+                .catch((err) => console.log(err.message));
+        });
+
         listItem.appendChild(title);
         listItem.appendChild(deleteButton);
         todoList.appendChild(listItem);
-    })
-};
+    });
+}
 
 printTodos();
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log('form submitted.', form.title.value);
-    let length = todos.length;
+if(form)
+{
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    let newTodo = {
-        title: form.title.value,
-        completed: false,
-        id: length,
-    };
+        const titleInput = document.querySelector("#title");
 
-    todos.push(newTodo);
-    console.log(todos);
-    printTodos();
-});
+        if(titleInput) {
 
+            const insertTodo = {
+                title: form.title.value
+            }
+            console.log(insertTodo);
 
+            const url = '/api/todos';
+
+            fetch(url, {
+                    method: "POST",
+                    headers: 
+                        {
+                            'Content-Type': 'application/json'
+                        },
+                    body: JSON.stringify(
+                        insertTodo
+                    )}
+                )
+                .then(res => printTodos())
+                .catch(err => console.log(err.message));
+            }
+        })
+}
 
 
 
